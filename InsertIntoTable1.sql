1ac5a37f650c1f91b3721aff7d236d09d8379d16
t-wen <30753500+t-wen@users.noreply.github.com>
2018-06-21 23:30:38 +0800

    Initial commit

---
+++ InsertIntoTable1.sql
@@ -0,0 +1,2 @@
+INSERT INTO Persons (WechartName, PhoneNumber, Password)
+VALUES ('Tracy', 13650931750, 13650931750);
---
+++ TablePersons.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Persons
+(
+PersonID int NOT NULL AUTO_INCREMENT,
+WechartName varchar(255) NOT NULL,
+PhoneNumber varchar(255),
+Password varchar(255),
+PRIMARY KEY (PersonID)
+)
