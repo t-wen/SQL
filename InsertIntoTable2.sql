848eedd9afa062c3c54b56c62a6eb5618d19bf56
t-wen <30753500+t-wen@users.noreply.github.com>
2018-06-22 23:34:48 +0800

    tt

---
+++ InsertIntoTable2.sql
@@ -0,0 +1,2 @@
+INSERT INTO Persons (WechartName, PhoneNumber, Password)
+VALUES ('xu', 13650931475, 13650931475);
--- TablePersons.sql
+++ TablePersons.sql
@@ -2,7 +2,7 @@ CREATE TABLE Persons
 (
 PersonID int NOT NULL AUTO_INCREMENT,
 WechartName varchar(255) NOT NULL,
-PhoneNumber varchar(255),
+PhoneNumber varchar(255) NOT NULL,
 Password varchar(255),
 PRIMARY KEY (PersonID)
 )
